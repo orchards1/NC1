1bfa52e5de46cb7f95c248686d74138bbd3b1647
Michael <jagosikat@yahoo.co.id>
2019-05-20 16:42:34 +0700

    Mantappp

--- NanoChallenge1/GameScene.swift
+++ NanoChallenge1/GameScene.swift
@@ -9,81 +9,121 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene ,SKPhysicsContactDelegate {
+    let Umbrella = SKSpriteNode(imageNamed: "Umbrella")
+    var rainBGM : SKAudioNode!
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    
     
     override func didMove(to view: SKView) {
+        backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let gamebackgroundmusic = SKAudioNode(fileNamed: "RainBGM.mp3")
+        SpawnUmbrella()
+        addChild(gamebackgroundmusic)
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.green
+//
+//            self.addChild(n)
+//        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.blue
+//            self.addChild(n)
+//        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.red
+//            self.addChild(n)
+//        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+//        if let label = self.label {
+//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
+//
+//        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
+        for touch in (touches )
+        {
+            let location = touch.location(in: self)
+
+            
+            moveUmbrella(location: location)
+    
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in (touches )
+        {
+            let location = touch.location(in: self)
+            
+            
+            moveUmbrella(location: location)
+            
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for touch in (touches )
+        {
+            let location = touch.location(in: self)
+            
+            
+            moveUmbrella(location: location)
+            
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for touch in (touches )
+        {
+            let location = touch.location(in: self)
+            moveUmbrella(location: location)
+            
+        }
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    func SpawnUmbrella(){
+        
+        Umbrella.size = CGSize(width: 100, height: 100)
+        Umbrella.setScale(1)
+        Umbrella.zPosition = 1
+        Umbrella.position = CGPoint(x: 90 , y:20)
+        self.addChild(Umbrella)
+        
+    }
+    
+    func moveUmbrella(location: CGPoint) {
+    let UmbrellaSpeed = frame.size.width * 5.0
+    let moveDifference = CGPoint(x: location.x - Umbrella.position.x, y: location.y - Umbrella.position.y)
+    let distanceToMove = sqrt(moveDifference.x * moveDifference.x + moveDifference.y * moveDifference.y)
+    let moveDuration = distanceToMove / UmbrellaSpeed
+    let moveAction = SKAction.move(to: location, duration:(TimeInterval(moveDuration)))
+    Umbrella.run(moveAction)
+    }
+    
 }

02d02eb60e96aab921001a74e5e1f5c108667a0b
Michael <jagosikat@yahoo.co.id>
2019-05-26 04:24:59 +0700

    Latest

--- NanoChallenge1/GameScene.swift
+++ NanoChallenge1/GameScene.swift
@@ -13,75 +13,39 @@ import Foundation
 
 class GameScene: SKScene ,SKPhysicsContactDelegate {
     let Umbrella = SKSpriteNode(imageNamed: "Umbrella")
-    let Awan = SKSpriteNode(imageNamed: "Awan")
+    let Text = SKSpriteNode(imageNamed: "Text")
     let fire = SKEmitterNode(fileNamed: "Fire")
+    let stone = SKSpriteNode(imageNamed: "Stone")
     let motionManager: CMMotionManager = CMMotionManager()
+    var destX:CGFloat  = 0.0
     
-    var timer = Timer()
-    
-    func move(){
-        var number = Int.random(in: 10 ... 50)
-        let rightorleft = Bool.random()
-        if(rightorleft == true)
-        {
-            moveright(asd: number)
-        }
-        else
-        {
-            moveleft(asd: number)
-        }
-    }
-    
-    var rainBGM : SKAudioNode!
-    func moveleft(asd: Int)
-    {
-        let UmbrellaSpeed = frame.size.width * 5.0
-        let distanceToMove = CGFloat(asd)
-        let moveDuration = distanceToMove / UmbrellaSpeed
-        let moveleft = SKAction.moveTo(x: Umbrella.position.x - distanceToMove, duration: TimeInterval(moveDuration))
-        
-        Umbrella.run(moveleft)
-    }
-    func moveright(asd: Int)
-    {
-        let UmbrellaSpeed = frame.size.width * 5.0
-        
-        let distanceToMove = CGFloat(asd)
-        let moveDuration = distanceToMove / UmbrellaSpeed
-         let moveright = SKAction.moveTo(x: Umbrella.position.x + distanceToMove, duration: TimeInterval(moveDuration))
-        Umbrella.run(moveright)
-    }
     override func didMove(to view: SKView) {
         
-        timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { [weak self] _ in
-            // do something here
-//            self?.move()
-            print()
-        }
         motionManager.startAccelerometerUpdates()
+        
         backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         
         let gamebackgroundmusic = SKAudioNode(fileNamed: "RainBGM.mp3")
-        
-        // Get node of object to move
-        let paddle = Umbrella
-        
-        // Get MotionManager data
-        if let data = motionManager.accelerometerData {
-            
-            // Only get use data if it is "tilt enough"
-            if (fabs(data.acceleration.x) > 0.2) {
-                
-                // Apply force to the moving object
-                paddle.physicsBody!.applyForce(CGVector( dx: 40 *  CGFloat(data.acceleration.x),dy: 0))
-                
-            }
-        }
-        
+        SpawnText()
+        SpawnStone()
         SpawnUmbrella()
         SpawnFire()
         addChild(gamebackgroundmusic)
         fireOff()
+        
+        if motionManager.isAccelerometerAvailable {
+            
+            motionManager.accelerometerUpdateInterval = 0.01
+            motionManager.startAccelerometerUpdates(to: .main) {
+                (data, error) in
+                guard let data = data, error == nil else {
+                    return
+                }
+                
+                let currentX = self.Umbrella.position.x
+                self.destX = currentX + CGFloat(data.acceleration.x * 500)
+            }
+        }
         
     }
     
@@ -102,7 +66,7 @@ class GameScene: SKScene ,SKPhysicsContactDelegate {
     func FireOn()
     {
         
-        UIView.animate(withDuration: 10.0, animations: { self.fire?.alpha = 1
+        UIView.animate(withDuration: 10.0, animations: { self.fire?.alpha = 0.5
             self.fire?.particleAlpha = 1
             self.fire?.particleLifetime = 2.5
         }, completion:{
@@ -111,58 +75,28 @@ class GameScene: SKScene ,SKPhysicsContactDelegate {
         })
     }
     func touchDown(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.green
-//
-//            self.addChild(n)
-//        }
+
         
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.blue
-//            self.addChild(n)
-//        }
+
         
     }
     
     func touchUp(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.red
-//            self.addChild(n)
-//        }
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-//        if let label = self.label {
-//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-//        }
-//
-//        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-        
         for touch in (touches )
         {
             let location = touch.location(in: self)
-            
-            if(location.x < 0 )
-            {
-                moveleft(asd: 10)
-            }
-            else
-            {
-                moveright(asd: 10)
-            }
-         
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
         for touch in (touches )
         {
               let location = touch.location(in: self)
@@ -176,14 +110,6 @@ class GameScene: SKScene ,SKPhysicsContactDelegate {
         {
               let location = touch.location(in: self)
             
-//            if(location.x <= 70.0 && location.x >= -70)
-//            {
-//                fire?.isHidden = false
-//            }
-//            else{
-//                fire?.isHidden = true
-//            }
-            
         }
     }
     
@@ -196,22 +122,25 @@ class GameScene: SKScene ,SKPhysicsContactDelegate {
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
+        if(destX > 350 )
+        {
+            destX = 320
+        }
+            
+        else if(destX < -350)
+        {
+            destX = -320
+        }
+        
+            let action = SKAction.moveTo(x: destX, duration: 1)
+            Umbrella.run(action)
+        
         if(Umbrella.position.x <= 60 && Umbrella.position.x >= -60)
         {
             FireOn()
         }
-//        else if((Umbrella.position.x > 60 && Umbrella.position.x <= 65 ) || Umbrella.position.x >= -65 && Umbrella.position.x < -60)
-//        {
-//            if(fire?.isHidden == true)
-//            {
-//                NoFire()
-//            }
-//            else
-//            {
-//                fireOff()
-//            }
-//        }
+            
         else
         {
             fireOff()
@@ -221,50 +150,39 @@ class GameScene: SKScene ,SKPhysicsContactDelegate {
     func SpawnUmbrella(){
         
         Umbrella.size = CGSize(width: 100, height: 100)
-        Umbrella.alpha = 0.5
-        Umbrella.setScale(1)
+        Umbrella.alpha = 0.8
+        Umbrella.setScale(1.2)
         Umbrella.zPosition = 2
-        Umbrella.position = CGPoint(x:130 , y:-600)
+        Umbrella.position = CGPoint(x:130 , y:-550)
         self.addChild(Umbrella)
     }
-//    func SpawnAwan(){
-//
-//        Awan.size = CGSize(width: 200, height: 100)
-//        Awan.alpha = 0.5
-//        Awan.setScale(1)
-//        Awan.zPosition = 1
-//        Awan.position = CGPoint(x: 110 , y:400)
-//        self.addChild(Awan)
-//
-//    }
+    func SpawnText()
+    {
+        Text.size = CGSize(width: 255, height: 77)
+        Text.alpha = 0.8
+        Text.setScale(1.5)
+        Text.zPosition = 2
+        Text.position = CGPoint(x:0 , y:200)
+        self.addChild(Text)
+    }
+    func SpawnStone(){
+
+        stone.size = CGSize(width: 133, height: 64)
+        stone.alpha = 1
+        stone.setScale(0.7)
+        stone.zPosition = 0
+        stone.position = CGPoint(x: 0 , y:-630)
+        self.addChild(stone)
+
+    }
     func SpawnFire(){
         
         fire!.alpha = 0.5
         fire!.setScale(1)
         fire!.zPosition = 1
-        fire!.position = CGPoint(x:0 , y:-650)
+        fire!.position = CGPoint(x:0 , y:-620)
         self.addChild(fire!)
     }
-   
-    
-    
-//    func moveFire(location: CGPoint) {
-//        let fireSpeed = frame.size.width * 5.0
-//        let moveDifference = CGPoint(x: location.x - fire!.position.x, y: location.y - fire!.position.y)
-//        let distanceToMove = sqrt(moveDifference.x * moveDifference.x + moveDifference.y * moveDifference.y)
-//        let moveDuration = distanceToMove / fireSpeed
-//        let moveAction = SKAction.move(to: location, duration:(TimeInterval(moveDuration)))
-//        fire!.run(moveAction)
-//    }
-    func moveUmbrella(location: CGPoint)
-    {
-    
-//    let moveAction = SKAction.move(to: location, duration:(TimeInterval(moveDuration)))
-//
- 
-       
-    }
-//
 
     
 }
